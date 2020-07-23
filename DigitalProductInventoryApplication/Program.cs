b57f6ebac591c4079fd4b4cc71dd9403ea6d6037
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2020-07-23 13:04:48 +0200

    Update Program.cs

--- DigitalProductInventoryApplication/Program.cs
+++ DigitalProductInventoryApplication/Program.cs
@@ -160,7 +160,7 @@ namespace DigitalProductInventoryApplication
     }
 
     public static class FactoryPattern<T, U> where T:class,U,new()
-                                             where U:class,IPrimaryProperties
+                                             where U:IPrimaryProperties
     {
         public static U GetInstance()
         {
